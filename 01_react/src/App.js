9d218aa5023638d6a91c4cc4fb1b8e884291f161
kuba <adamiak.dev@gmail.com>
2022-11-22 11:16:13 +0100

    init

---
+++ 01_react/src/App.js
@@ -0,0 +1,30 @@
+const Pet = (args) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, args.name),
+        React.createElement("h2", {}, args.animal),
+        React.createElement("h2", {}, args.breed),
+    ])
+}
+
+const App = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {id: "brand"}, "Adopt me!"),
+        React.createElement(Pet, {
+            name: "Luna",
+            animal: "Dog", 
+            breed: "Havanian"
+        }),
+        React.createElement(Pet, {
+            name: "Pepper",
+            animal: "Parrot", 
+            breed: "Cockatiel"
+        }),
+        React.createElement(Pet, {
+            name: "Doink",
+            animal: "Cat", 
+            breed: "Persian"
+        }),
+    ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
